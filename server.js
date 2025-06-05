63fcf9a14c5c1452d84b9ebe3d8505719b4394f8
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2025-06-05 10:08:53 +0400

    feat: add transaction table

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const schema = fs.readFileSync('schema.sql', 'utf8');
 
 const app = express();
 
-
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'zappit',
+  multipleStatements: true, // allow multiple statements for schema creation
 });
 
 connection.connect((err) => {
